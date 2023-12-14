ca79073f2b47dd462eb4ebf81f65e6f93c5dc47f
Soulbolt <soulbolt@gmail.com>
2023-12-14 14:19:03 -0700

    Created sneak attributes, list for position, size, and place them next to each other on the drawing board..

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Turtle, Screen
 
 # Set up screen size, title, background color
@@ -5,6 +6,19 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Sneaky Snake")
+
+# Set up turtle size and color
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake_size = []
+size = 0
+for size in starting_positions:
+    new_snake = Turtle(shape="square")
+    new_snake.penup()
+    new_snake.color("white")
+    new_snake.goto(size)
+    snake_size.append(new_snake)
+
+
 
 
 
