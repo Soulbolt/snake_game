aa75826003ced6d08850a20bec189fbde2fdde85
Soulbolt <soulbolt@gmail.com>
2023-12-14 13:16:02 -0700

    Added code to set up screen size, title, and background color.

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+# Set up screen size, title, background color
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Sneaky Snake")
+
+
+
+screen.exitonclick()
