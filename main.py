5ec7cb306d0ded7c37b189f9b1351a240ffa4e8b
Soulbolt <soulbolt@gmail.com>
2023-12-14 21:08:48 -0700

    refactor code to create snake class with all methods of the respective snake functions.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
-import turtle
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 
 # Set up screen size, title, background color
 screen = Screen()
@@ -14,64 +14,41 @@ right_bound = 280
 top_bound = 280
 bottom_bound = -280
 
-# Set up turtle size and color
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-snake_size = []
-
-
-# Set up sneak default position
-# def rest_position():
-for location in starting_positions:
-    new_snake = Turtle(shape="square")
-    new_snake.penup()
-    new_snake.color("white")
-    new_snake.goto(location)
-    snake_size.append(new_snake)
-
-snake_head = snake_size[0]
-
-
-def turn_left():
-    snake_head.left(90)
-
-
-def turn_right():
-    snake_head.right(90)
-
-
-# Key bindings for movement actions
+snakes = Snake()
 
 # Create a while loop to keep game going
 game_is_on = True
 while game_is_on:
     # Set functions and listening actions for navigation keys
+    # Key bindings for movement actions
+    # ran_loc = random_dots(random_x_position, random_y_position)
     screen.listen()
-    screen.onkey(turn_right, "d")
-    screen.onkey(turn_left, "a")
+    screen.onkey(snakes.turn_right(90), "d")
+    screen.onkey(snakes.turn_left(90), "a")
     # Make snake move
     screen.update()  # Update animation
     time.sleep(0.1)  # delay of animation
 
-    for segment_num in range(len(snake_size) - 1, 0, -1):
-        new_x = snake_size[segment_num - 1].xcor()  # Previous snake segment coordinates
-        new_y = snake_size[segment_num - 1].ycor()
-        snake_size[segment_num].goto(new_x, new_y)
+    snakes.move()
 
-    if snake_head.xcor() > right_bound or snake_head.xcor() < left_bound:
+    if snakes.snake_size[0].xcor() > right_bound or snakes.snake_size[0].xcor() < left_bound:
         game_is_on = False
         print("You lose! You've crashed into Trumps Wall!")
 
-    if snake_head.ycor() > top_bound or snake_head.ycor() < bottom_bound:
+    if snakes.snake_size[0].ycor() > top_bound or snakes.snake_size[0].ycor() < bottom_bound:
         game_is_on = False
         print("You lose! You've crashed into Trumps Wall!")
 
-    snake_head.forward(20)
+    # if ran_loc.pos() == snake_head.pos():
+    #     ran_loc.hideturtle()
+    #     ran_loc.reset()
+    #     random_dots(random_x_position, random_y_position)
 
     if not game_is_on:
         try_again = screen.textinput("Try again?", "Enter yes or no").lower()
         if try_again == "yes":
             game_is_on = True
-            snake_head.home()
+            snakes.reset_location()
         elif try_again == "no":
             break
         else:
---
+++ snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+import random
+
+
+def random_dots(x, y):
+    new_dot = Turtle(shape="circle")
+    new_dot.penup()
+    new_dot.color("SkyBlue")
+    # new_dot.hideturtle()
+    new_dot.goto(x, y)
+    return new_dot
+
+
+# Constants
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+ANGLE = 90
+
+
+class Snake:
+    def __init__(self):
+        self.snake_size = []
+        self.create_snakes()
+        self.random_x_position = random.randint(-260, 260)
+        self.random_y_position = random.randint(-260, 260)
+
+    def create_snakes(self):
+        for location in STARTING_POSITIONS:
+            new_snake = Turtle(shape="square")
+            new_snake.penup()
+            new_snake.color("white")
+            new_snake.goto(location)
+            self.snake_size.append(new_snake)
+
+    def move(self):
+        for segment_num in range(len(self.snake_size) - 1, 0, -1):  # movement (Start, stop, step)
+            new_x = self.snake_size[segment_num - 1].xcor()  # Previous snake segment coordinates
+            new_y = self.snake_size[segment_num - 1].ycor()
+            self.snake_size[segment_num].goto(new_x, new_y)
+        self.snake_size[0].forward(MOVE_DISTANCE)
+
+    def turn_left(self):
+        self.snake_size[0].left(ANGLE)
+
+    def turn_right(self):
+        self.snake_size[0].right(ANGLE)
+
+    def reset_location(self):
+        self.snake_size[0].home()
