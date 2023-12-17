11c02ee9a59b73148c31500a2883a40dcb2632ff
Soulbolt <soulbolt@gmail.com>
2023-12-16 20:32:31 -0700

    added code to open file to read and write high score and  keep data in local and load last high score when game is booted up again.

--- scoreboard.py
+++ scoreboard.py
@@ -11,7 +11,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data") as data:
+            self.high_score = int(data.read())
         self.goto(0, 565)
         self.color("white")
         self.penup()
@@ -26,6 +27,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data", "w") as data:
+                data.write(str(self.high_score))
         self.score = 0
         self.update_score()
 
